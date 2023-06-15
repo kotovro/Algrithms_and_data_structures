5dda55e7585cd269b9100a9dd6bc6bafec8e573c
Kotvoro <kotvoro@gmail.com>
2023-06-15 07:31:57 +0300

    Task 06: implemented demo for AVL and RB trees map

--- Task06_TreeMapIterator/src/ru/vsu/cs/course1/tree/demo/TreeDemoFrame.java
+++ Task06_TreeMapIterator/src/ru/vsu/cs/course1/tree/demo/TreeDemoFrame.java
@@ -6,13 +6,10 @@ import com.intellij.uiDesigner.core.Spacer;
 import ru.vsu.cs.course1.tree.SimpleBinaryTree;
 import ru.vsu.cs.course1.tree.BinaryTreePainter;
 import ru.vsu.cs.course1.tree.BinaryTree;
-import ru.vsu.cs.course1.tree.bst.BSTree;
 import ru.vsu.cs.course1.tree.bst.BSTreeMap;
-import ru.vsu.cs.course1.tree.bst.SimpleBSTree;
 import ru.vsu.cs.course1.tree.bst.SimpleBSTreeMap;
 import ru.vsu.cs.course1.tree.bst.avl.AVLTreeMap;
 import ru.vsu.cs.course1.tree.bst.rb.RBTreeMap;
-import ru.vsu.cs.util.ArrayUtils;
 import ru.vsu.cs.util.DemoUtils;
 import ru.vsu.cs.util.SwingUtils;
 
@@ -20,8 +17,9 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
-import java.util.Arrays;
 import java.util.Map;
 
 public class TreeDemoFrame extends JFrame {
@@ -32,22 +30,23 @@ public class TreeDemoFrame extends JFrame {
     private JTextField textFieldValues;
     private JSpinner spinnerRandomCount;
     private JButton buttonRandomGenerate;
-    private JButton buttonSortValues;
+
     private JButton buttonMakeBSTree2;
-    private JButton buttonAddValue;
-    private JButton buttonRemoveValue;
     private JPanel panelPaintArea;
     private JButton buttonSaveImage;
     private JCheckBox checkBoxTransparent;
-    private JSpinner spinnerSingleValue;
+
     private JTextField textFieldFrom;
     private JTextField textFieldTo;
+    private JButton buttonCreateAvlTreeMap;
+    private JButton buttonCreateRBTreeMap;
 
     private JMenuBar menuBarMain;
     private JPanel paintPanel = null;
     private JFileChooser fileChooserSave;
 
     BinaryTree<BSTreeMap.MapTreeEntry<Integer, String>> tree = new SimpleBinaryTree<>();
+    BSTreeMap<Integer, String> treeMap = null;
 
 
     public TreeDemoFrame() {
@@ -86,56 +85,22 @@ public class TreeDemoFrame extends JFrame {
         fileChooserSave.setApproveButtonText("Save");
 
         spinnerRandomCount.setValue(30);
-        spinnerSingleValue.setValue(10);
 
         buttonRandomGenerate.addActionListener(actionEvent -> {
             int size = ((Integer) spinnerRandomCount.getValue()).intValue();
             textFieldValues.setText(DemoUtils.getRandomPairs(size));
         });
-        buttonSortValues.addActionListener(actionEvent -> {
-            try {
-                int[] arr = ArrayUtils.toIntArray(textFieldValues.getText());
-                Arrays.sort(arr);
-                textFieldValues.setText(ArrayUtils.toString(arr));
-            } catch (Exception ex) {
-                SwingUtils.showErrorMessageBox(ex);
-            }
-        });
+
 
         buttonMakeBSTree2.addActionListener(actionEvent -> {
             try {
-                makeBSTFromValues(new SimpleBSTree<>(DemoUtils.stringToEntry));
+                this.treeMap = new SimpleBSTreeMap<Integer, String>();
+                makeTreeMapFromValues(treeMap);
             } catch (Exception ex) {
                 SwingUtils.showErrorMessageBox(ex);
             }
         });
-
-//        buttonAddValue.addActionListener(actionEvent -> {
-//            if (!(tree instanceof BSTree)) {
-//                SwingUtils.showInfoMessageBox("Текущее дерево не является деревом поиска!");
-//                return;
-//            }
-//            try {
-//                int value = Integer.parseInt(spinnerSingleValue.getValue().toString());
-//                ((BSTree<Integer>) tree).put(value);
-//                repaintTree();
-//            } catch (Exception ex) {
-//                SwingUtils.showErrorMessageBox(ex);
-//            }
-//        });
-//        buttonRemoveValue.addActionListener(actionEvent -> {
-//            if (!(tree instanceof BSTree)) {
-//                SwingUtils.showInfoMessageBox("Текущее дерево не является деревом поиска!");
-//                return;
-//            }
-//            try {
-//                int value = Integer.parseInt(spinnerSingleValue.getValue().toString());
-//                ((BSTree<Integer>) tree).remove(value);
-//                repaintTree();
-//            } catch (Exception ex) {
-//                SwingUtils.showErrorMessageBox(ex);
-//            }
-//        });
+        
 
 
         buttonSaveImage.addActionListener(actionEvent -> {
@@ -157,16 +122,41 @@ public class TreeDemoFrame extends JFrame {
 
         buttonGetIterator.addActionListener(actionEvent -> {
             showSystemOut(() -> {
-                System.out.println("Итератор:");
+                System.out.println("TreeMap:");
+                for (BSTreeMap.Entry<Integer, String> node : treeMap.entrySet()) {
+                    System.out.println(node.toString());
+                }
+                System.out.println();
+                System.out.println("Итератор С-ПО:");
                 int keyFrom = Integer.parseInt(textFieldFrom.getText());
                 int keyTo = Integer.parseInt(textFieldTo.getText());
-                BSTreeMap.MapTreeEntry<Integer, String> from = new BSTreeMap.MapTreeEntry<>(keyFrom, null);
-                BSTreeMap.MapTreeEntry<Integer, String> to = new BSTreeMap.MapTreeEntry<>(keyTo, null);
-                for (BSTreeMap.MapTreeEntry<Integer, String> node : ((BSTree<BSTreeMap.MapTreeEntry<Integer, String>>) tree)
-                        .iterateFromTo(from, to)) {
+                for (BSTreeMap.MapTreeEntry<Integer, String> node : treeMap.iterateFromTo(keyFrom, keyTo)) {
                     System.out.println(node.toString());
                 }
             });
+        });
+        buttonCreateAvlTreeMap.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    treeMap = new AVLTreeMap<>();
+                    makeTreeMapFromValues(treeMap);
+                } catch (Exception ex) {
+                    SwingUtils.showErrorMessageBox(ex);
+                }
+
+            }
+        });
+        buttonCreateRBTreeMap.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    treeMap = new RBTreeMap<>();
+                    makeTreeMapFromValues(treeMap);
+                } catch (Exception ex) {
+                    SwingUtils.showErrorMessageBox(ex);
+                }
+            }
         });
     }
 
@@ -228,15 +218,15 @@ public class TreeDemoFrame extends JFrame {
     /**
      * Заполнить дерево добавлением всех элементов (textFieldValues)
      *
-     * @param tree Дерево
+     * @param map Дерево
      */
-    private void makeBSTFromValues(BSTree<BSTreeMap.MapTreeEntry<Integer, String>> tree) {
+    private void makeTreeMapFromValues(BSTreeMap<Integer, String> map) {
         BSTreeMap.MapTreeEntry<Integer, String>[] values = DemoUtils.toPairsArray(textFieldValues.getText());
-        tree.clear();
+        map.clear();
         for (BSTreeMap.MapTreeEntry<Integer, String> v : values) {
-            tree.put(v);
+            map.put(v.key, v.value);
         }
-        this.tree = tree;
+        this.tree = map.getTree();
         repaintTree();
     }
 
@@ -269,53 +259,44 @@ public class TreeDemoFrame extends JFrame {
         textFieldValues.setText("86=qo,79=md,30=uv,43=vc,5=ah,30=of,28=lo,13=ch,9=fo,59=ey,51=gl,2=oo,47=dr,29=ir,28=wb");
         panel2.add(textFieldValues, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JPanel panel3 = new JPanel();
-        panel3.setLayout(new GridLayoutManager(1, 5, new Insets(0, 0, 0, 0), -1, -1));
+        panel3.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
         panel2.add(panel3, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         spinnerRandomCount = new JSpinner();
         panel3.add(spinnerRandomCount, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, new Dimension(80, -1), new Dimension(80, -1), new Dimension(80, -1), 0, false));
         buttonRandomGenerate = new JButton();
         buttonRandomGenerate.setText("Сгенерировать");
         panel3.add(buttonRandomGenerate, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        buttonSortValues = new JButton();
-        buttonSortValues.setText("Упорядочить");
-        panel3.add(buttonSortValues, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final Spacer spacer1 = new Spacer();
-        panel3.add(spacer1, new GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JLabel label1 = new JLabel();
         label1.setText("cлучайных пар");
         panel3.add(label1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel4 = new JPanel();
-        panel4.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel4.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
         panel2.add(panel4, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         buttonMakeBSTree2 = new JButton();
-        buttonMakeBSTree2.setText("Построить дерево поиска");
+        buttonMakeBSTree2.setText("Построить  TreeMap и отобразить дерево поиска");
         panel4.add(buttonMakeBSTree2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        buttonCreateAvlTreeMap = new JButton();
+        buttonCreateAvlTreeMap.setText("Построить  AVLTreeMap и отобразить дерево поиска");
+        panel4.add(buttonCreateAvlTreeMap, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel5 = new JPanel();
-        panel5.setLayout(new GridLayoutManager(1, 4, new Insets(0, 0, 0, 0), -1, -1));
+        panel5.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         panel2.add(panel5, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        final Spacer spacer2 = new Spacer();
-        panel5.add(spacer2, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        buttonAddValue = new JButton();
-        buttonAddValue.setText("Добавить");
-        panel5.add(buttonAddValue, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        buttonRemoveValue = new JButton();
-        buttonRemoveValue.setText("Удалить");
-        panel5.add(buttonRemoveValue, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        spinnerSingleValue = new JSpinner();
-        panel5.add(spinnerSingleValue, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, new Dimension(80, -1), new Dimension(80, -1), new Dimension(80, -1), 0, false));
+        buttonCreateRBTreeMap = new JButton();
+        buttonCreateRBTreeMap.setText("Построить  RBTreeMap и отобразить дерево поиска");
+        panel5.add(buttonCreateRBTreeMap, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         panelPaintArea = new JPanel();
         panelPaintArea.setLayout(new BorderLayout(0, 0));
         panel1.add(panelPaintArea, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        final Spacer spacer3 = new Spacer();
-        panelPaintArea.add(spacer3, BorderLayout.CENTER);
+        final Spacer spacer1 = new Spacer();
+        panelPaintArea.add(spacer1, BorderLayout.CENTER);
         final JPanel panel6 = new JPanel();
         panel6.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(panel6, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         buttonSaveImage = new JButton();
         buttonSaveImage.setText("Сохранить изображение в SVG");
         panel6.add(buttonSaveImage, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final Spacer spacer4 = new Spacer();
-        panel6.add(spacer4, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel6.add(spacer2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         checkBoxTransparent = new JCheckBox();
         checkBoxTransparent.setText("прозрачность");
         panel6.add(checkBoxTransparent, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -350,4 +331,7 @@ public class TreeDemoFrame extends JFrame {
         return panelMain;
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
